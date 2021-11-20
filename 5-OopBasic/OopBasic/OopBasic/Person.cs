e8c79802205b37bb7add9473dc077ca1d59ece16
Berkan <37382871+berkanuz@users.noreply.github.com>
2021-11-20 23:33:11 +0300

    .
    .

---
+++ 5-OopBasic/OopBasic/OopBasic/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopBasic
+{
+    public class Person
+    {
+        public interface IPerson
+        {
+            public string Sex { get; set; }
+            public string Branch { get; set; }
+        }
+
+        public class Student : IPerson
+        {
+            public string Sex { get; set; }
+            public string Branch { get; set; }
+            public string Brand = "Student";
+
+        }
+
+        public class Teacher : IPerson
+        {
+            public string Sex { get; set; }
+            public string Branch { get; set; }
+            public string Brand = "Teacher";
+        }
+
+
+        public class Student1 : Student
+        {
+            public int Id = 1;
+            public string Name = "Ahmet";
+            public string Nationality = "Turkey";
+            public string Scholarship = "Yes";
+        }
+
+        public class Student2 : Student
+        {
+            public int Id = 2;
+            public string Name = "Mehmet";
+            public string Nationality = "Canada";
+            public string Scholarship = "No";
+
+        }
+
+        public class Teacher1 : Teacher
+        {
+            public int Id = 3;
+            public string Name = "Ayse";
+            public string Nationality = "El Salvador";
+        }
+    }
+}
---
+++ 5-OopBasic/OopBasic/OopBasic/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static OopBasic.Person;
+
+namespace OopBasic
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student1 student = new Student1() { Sex = "Male", Branch = "Math"};
+            Student2 student2 = new Student2() { Sex = "Male", Branch = "Computer Science" };
+            Teacher1 teacher = new Teacher1() { Sex = "Famale", Branch = "Physics" };
+
+            Console.WriteLine("**STUDENT**");
+
+            Console.WriteLine("Id: "+ student.Id);
+            Console.WriteLine("Sex: "+ student.Sex);
+            Console.WriteLine("Branch: "+ student.Branch);
+            Console.WriteLine("Brand: "+ student.Brand);
+            Console.WriteLine("Nationality: "+ student.Nationality);
+            Console.WriteLine("ScholarShip: "+ student.Scholarship);
+
+            Console.WriteLine("**STUDENT**");
+
+            Console.WriteLine("Id: " + student2.Id);
+            Console.WriteLine("Sex: " + student2.Sex);
+            Console.WriteLine("Branch: " + student2.Branch);
+            Console.WriteLine("Brand: " + student2.Brand);
+            Console.WriteLine("Nationality: " + student2.Nationality);
+            Console.WriteLine("ScholarShip: " + student2.Scholarship);
+
+            Console.WriteLine("**TEACHER**");
+
+            Console.WriteLine("Id: " + teacher.Id);
+            Console.WriteLine("Sex: " + teacher.Sex);
+            Console.WriteLine("Branch: " + teacher.Branch);
+            Console.WriteLine("Brand: " + teacher.Brand);
+            Console.WriteLine("Nationality: " + teacher.Nationality);
+
+
+            Console.ReadLine();
+        }
+    }
+}
